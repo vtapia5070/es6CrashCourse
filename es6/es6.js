42d33159dbbdcd96ce1966ea3877a0339958f728
victoria tapia <victoria_tapia@discovery.com>
2016-06-15 12:06:02 -0700

    Update Es6 summary of variables

---
+++ es6/es6.js
@@ -0,0 +1,11 @@
+/* Summary Notes here:
+ * See code in src/Main.js
+ * See demo in html browser console.
+ */
+
+/*
+ * Variables
+ * var - will allow variable hoisting
+ * let - block level variable declaration
+ * const - immutable variable declaration (although the value is not immutable and can be modified!)
+*/
