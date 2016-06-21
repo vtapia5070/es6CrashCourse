bea55ebd0fd17e8946609cf265a7e260891ad327
victoria tapia <victoria_tapia@discovery.com>
2016-06-21 16:41:57 -0700

    add default parameters lesson

---
+++ src/Es6_DefaultParameters.js
@@ -0,0 +1,25 @@
+// original
+function originalApplyDiscount(cost, discount) {
+  discount = discount || .10;
+
+  return cost - (cost * discount);
+}
+alert(originalApplyDiscount(100, .04)); // 60
+alert(originalApplyDiscount(100)); // 90
+
+function defaultDiscountRate () {
+  return .10;
+}
+
+function applyDiscountA(cost, discount = defaultDiscountRate()) {
+  return cost - (cost * discount);
+}
+
+function applyDiscountB(cost, discount = .10) {
+  return cost - (cost * discount);
+}
+
+
+alert(applyDiscountA(100));
+
+alert(applyDiscountB(100));
