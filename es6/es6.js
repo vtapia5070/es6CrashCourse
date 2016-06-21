517f77a39faa88ac179e2ad1eb2a76f681ba8c51
victoria tapia <victoria_tapia@discovery.com>
2016-06-21 16:24:38 -0700

    update arrow summary

--- es6/es6.js
+++ es6/es6.js
@@ -5,7 +5,19 @@
 
 /*
  * Variables
- * var - will allow variable hoisting
- * let - block level variable declaration
- * const - immutable variable declaration (although the value is not immutable and can be modified!)
-*/
+ */
+
+// var - will allow variable hoisting
+// let - block level variable declaration
+// const - immutable variable declaration (although the value is not immutable and can be modified!)
+
+
+/*
+ * Arrows
+ */
+
+// Parameters can be surrounded by opening and closing parenthesis
+// If one parameter exists, the parens are optional.
+// If two params exist, parens are required.
+// caveat: the value of the this keyword refers to the collection/function scope it is used within
+
