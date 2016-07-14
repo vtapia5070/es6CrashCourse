3a3ed6e1d0fdc2148c6b2dd88ff1b462abc1e60a
victoria tapia <victoria_tapia@discovery.com>
2016-07-14 14:29:21 -0700

    complete Rest and Spread lesson

--- src/Es6_restAndSpread.js
+++ src/Es6_restAndSpread.js
@@ -8,20 +8,23 @@ function sum (...numbers) {
   })
 }
 
-sum(1,2,3); // 6
+console.log('Ex 1.1', sum(1,2,3)); // 6
 
-sum(3,0); // 3
+// console.log('Ex 1.2', sum(3,0)); // 3
 
-sum(10, 2, 4, 6, 1); // 23
+// console.log('Ex 1.3', sum(10, 2, 4, 6, 1)); // 23
 
 /* Warning! */
-// If adding another parameter,
-// use the rest syntax as the second parameter.
-function sum2 (foo, ...numbers) {
-  return numbers.reduce(function (prev, current) {
-    return prev + current;
-  })
+// No other parameters will work with the rest syntax
+
+/*
+ * Spread Parameters
+ */
+
+function sum3 (x, y) {
+  return x + y;
 }
 
-sum("hi", 1, 6, 2); // "hi 9"
-sum("total is ", 3, 9, 1, 5, 0); // "total is 18"
+let nums = [1, 2];
+
+console.log(sum3(...nums));
