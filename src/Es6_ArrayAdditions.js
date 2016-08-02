01eeeda662294f124b31e57ea9ab52432abff7d2
victoria tapia <victoria_tapia@discovery.com>
2016-08-02 12:16:21 -0700

    comment console logs in previous lesosns

--- src/Es6_ArrayAdditions.js
+++ src/Es6_ArrayAdditions.js
@@ -4,13 +4,13 @@ var nums = [2, 4, 6, 8, 10];
 
 // ES5
 
-console.log(nums.indexOf(8)); // 3
-console.log(nums.indexOf(3)); // -1
+// console.log(nums.indexOf(8)); // 3
+// console.log(nums.indexOf(3)); // -1
 
 // ES6 - find first method
-console.log(nums.find(function (item) {
-  return item === 8;
-})); // 8
+// console.log(nums.find(function (item) {
+//   return item === 8;
+// })); // 8
 
 class User {
 
@@ -27,20 +27,20 @@ let users = [
   new User('Jane', false)
 ];
 
-console.log(users.find(user => user.isAdmin)); // Jack
-console.log(users.find(user => user.isAdmin).name); // Jack
+// console.log(users.find(user => user.isAdmin)); // Jack
+// console.log(users.find(user => user.isAdmin).name); // Jack
 
-// let items1 = ['jeff', 'jordan', 'way'].fill(); // ???
+// let items1 = ['jeff', 'jordan', 'way'].fill(); //???
 let items2 = ['jeff', 'jordan', 'way'].entries();
 let items3 = ['jeff', 'jordan', 'way'].values();
 let items4 = ['jeff', 'jordan', 'way'].keys();
 let items5 = ['jeff', 'jordan', 'way'].entries();
 
-for (let name of items2) console.log("entries - ", name);
-for (let name of items3) console.log("values - ", name);
-for (let name of items4) console.log("keys - ", name);
-for (let name of items5) console.log("entries - ", name);
+// for (let name of items2) console.log("entries - ", name);
+// for (let name of items3) console.log("values - ", name);
+// for (let name of items4) console.log("keys - ", name);
+// for (let name of items5) console.log("entries - ", name);
 
 // ES2017 ?
-console.log(nums.includes(8)); // true
+// console.log(nums.includes(8)); // true
 
--- src/Es6_StringAdditions.js
+++ src/Es6_StringAdditions.js
@@ -4,38 +4,38 @@ let title = 'Red Rising';
 
 // ES5
 if (title.indexOf('R') === 0) {
-  console.log('R is the first letter');
+  // console.log('R is the first letter');
 }
 
 if (title.indexOf('Blue') === -1) {
-  console.log('This book does not begin with Blue');
+  // console.log('This book does not begin with Blue');
 }
 
 // ES6 Includes
 if (title.includes('R')) {
-  console.log('R is the first letter');
+  // console.log('R is the first letter');
 }
 
 if (title.includes('Blue')) {
-  console.log('This book does not begin with Blue');
+  // console.log('This book does not begin with Blue');
 }
 
 // ES6 Starts with
 if (title.startsWith('Red')) {
-  console.log('This book starts with Red');
+  // console.log('This book starts with Red');
 }
 
 if (title.startsWith('i', 5)) {
-  console.log('It does');
+  // console.log('It does');
 }
 
 // ES6 Ends with
 if (title.endsWith('Rising')) {
-  console.log('This book ends with Rising');
+  // console.log('This book ends with Rising');
 }
 
 // ES6 Repeat
 let str = 'tro';
-console.log(
-  str + 'lol'.repeat(100)
-);
+// console.log(
+//   str + 'lol'.repeat(100)
+// );
